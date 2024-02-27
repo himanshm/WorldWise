15cd5e11849e4d7e59c41fbf40e298001686def8
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-27 18:51:47 +0530

    Display City markers on map Interact with the map

--- src/components/app/Map.tsx
+++ src/components/app/Map.tsx
@@ -1,33 +1,84 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
 import styles from './Map.module.css';
-import { Position } from '../../contexts/CitiesContext';
-import { useState } from 'react';
+import { CityType } from '../../contexts/CitiesContext';
+import { useEffect, useState } from 'react';
+import { useCitiesContext } from '../../contexts/useCitiesContext';
+
+type Position = [lat: number, lng: number];
+
+type ChangeCenterTypes = {
+  position: Position;
+};
+
+function ChangeCenter({ position }: ChangeCenterTypes) {
+  const map = useMap();
+  // const { lat, lng } = position;
+  const newPosition: LatLngExpression = position;
+  map.setView(newPosition);
+
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
+
+  return null;
+}
 
 function Map() {
-  const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState<Position>({ lat: 40, lng: 0 });
-  // const [searchParams, setSearchParams] = useSearchParams();
+  const { cities } = useCitiesContext();
+  const [mapPosition, setMapPosition] = useState<Position>([40, 0]);
+  const [searchParams] = useSearchParams();
 
-  // const lat = searchParams.get('lat');
-  // const lng = searchParams.get('lng');
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  // const centerLat = mapLat && parseFloat(mapLat);
+  // const centerLng = mapLng && parseFloat(mapLng);
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([+mapLat, +mapLng]);
+  }, [mapLat, mapLng]);
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
-        center={[mapPosition.lat, mapPosition.lng]}
-        zoom={13}
+        center={mapPosition}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <Marker position={[mapPosition.lat, mapPosition.lng]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city: CityType) => (
+          <Marker
+            key={city.id}
+            position={[+city.position.lat, +city.position.lng]}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
